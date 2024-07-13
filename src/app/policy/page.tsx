b20123c0bcaf60c4a470f4e87ef78d07f84db725
minhvo0o <vdminh@penphy.edu.vn>
2024-07-13 22:26:15 +1000

    Update page title and description for WAM Master

--- src/app/policy/page.tsx
+++ src/app/policy/page.tsx
@@ -1,24 +1,188 @@
-import { CallToAction } from '@/components/CallToAction'
-import { Faqs } from '@/components/Faqs'
-import { Hero } from '@/components/Hero'
-import { Pricing } from '@/components/Pricing'
-import { PrimaryFeatures } from '@/components/PrimaryFeatures'
-import { Reviews } from '@/components/Reviews'
-import { SecondaryFeatures } from '@/components/SecondaryFeatures'
-
 export default function Policy() {
   return (
     <>
-      <div className="bg-bgPrimary h-screen flex-1">
-        <div className="mx-auto flex h-screen w-4/5 items-center">
+      <div className="bg-bgPrimary flex-1">
+        <div className="mx-auto flex w-4/5 py-24">
           <div className="flex-col space-y-6">
-            <h1 className="text-5xl font-bold">Contact</h1>
-            <p className="text-2xl">
-              Contact me at{' '}
-              <a href="mailto:minhvo.business@gmail.com" className="text-bg2">
+            <h1 className="text-5xl font-bold">Privacy Policy</h1>
+            <div className="flex-col space-y-4">
+              <p className="text-xl">
+                WAM Master is committed to providing quality services to you and
+                this policy outlines our ongoing obligations to you in respect
+                of how we manage your Personal Information.
+              </p>
+              <p className="text-xl">
+                We have adopted the Australian Privacy Principles (APPs)
+                contained in the Privacy Act 1988 (Cth) (the Privacy Act). The
+                NPPs govern the way in which we collect, use, disclose, store,
+                secure and dispose of your Personal Information.
+              </p>
+              <p className="text-xl">
+                A copy of the Australian Privacy Principles may be obtained from
+                the website of The Office of the Australian Information
+                Commissioner at https://www.oaic.gov.au/.
+              </p>
+            </div>
+            <h2 className="text-2xl font-bold">
+              What is Personal Information and why do we collect it?
+            </h2>
+            <div className="flex-col space-y-4">
+              <p className="text-xl">
+                Personal Information is information or an opinion that
+                identifies an individual. Examples of Personal Information we
+                collect includes names, addresses, email addresses, phone and
+                facsimile numbers.
+              </p>
+              <p className="text-xl">
+                This Personal Information is obtained in many ways including
+                [interviews, correspondence, by telephone and facsimile, by
+                email, via our website www.yourbusinessname.com.au, from your
+                website, from media and publications, from other publicly
+                available sources, from cookies- delete all that aren’t
+                applicable] and from third parties. We don’t guarantee website
+                links or policy of authorised third parties.
+              </p>
+              <p className="text-xl">
+                We collect your Personal Information for the primary purpose of
+                providing our services to you, providing information to our
+                clients and marketing. We may also use your Personal Information
+                for secondary purposes closely related to the primary purpose,
+                in circumstances where you would reasonably expect such use or
+                disclosure. You may unsubscribe from our mailing/marketing lists
+                at any time by contacting us in writing.
+              </p>
+              <p className="text-xl">
+                When we collect Personal Information we will, where appropriate
+                and where possible, explain to you why we are collecting the
+                information and how we plan to use it.
+              </p>
+            </div>
+            <h2 className="text-2xl font-bold">Sensitive Information</h2>
+            <div className="flex-col space-y-4">
+              <p className="text-xl">
+                Sensitive information is defined in the Privacy Act to include
+                information or opinion about such things as an individual's
+                racial or ethnic origin, political opinions, membership of a
+                political association, religious or philosophical beliefs,
+                membership of a trade union or other professional body, criminal
+                record or health information.
+              </p>
+              <p className="text-xl">
+                Sensitive information will be used by us only:
+                <ul className="text-xl">
+                  <li>For the primary purpose for which it was obtained</li>
+                  <li>
+                    For a secondary purpose that is directly related to the
+                    primary purpose
+                  </li>
+                  <li>
+                    With your consent; or where required or authorised by law.
+                  </li>
+                </ul>
+              </p>
+            </div>
+            <h2 className="text-2xl font-bold">Third Parties</h2>
+            <div className="flex-col space-y-4">
+              <p className="text-xl">
+                Where reasonable and practicable to do so, we will collect your
+                Personal Information only from you. However, in some
+                circumstances we may be provided with information by third
+                parties. In such a case we will take reasonable steps to ensure
+                that you are made aware of the information provided to us by the
+                third party.
+              </p>
+            </div>
+            <h2 className="text-2xl font-bold">
+              Disclosure of Personal Information
+            </h2>
+            <div className="flex-col space-y-4">
+              <p className="text-xl">
+                Your Personal Information may be disclosed in a number of
+                circumstances including the following:
+              </p>
+              <ul className="text-xl">
+                <li>
+                  Third parties where you consent to the use or disclosure
+                </li>
+                <li>Where required or authorised by law.</li>
+              </ul>
+            </div>
+            <h2 className="text-2xl font-bold">
+              Security of Personal Information
+            </h2>
+            <div className="flex-col space-y-4">
+              <p className="text-xl">
+                Your Personal Information is stored in a manner that reasonably
+                protects it from misuse and loss and from unauthorized access,
+                modification or disclosure.
+              </p>
+              <p className="text-xl">
+                When your Personal Information is no longer needed for the
+                purpose for which it was obtained, we will take reasonable steps
+                to destroy or permanently de-identify your Personal Information.
+                However, most of the Personal Information is or will be stored
+                in client files which will be kept by us for a minimum of 7
+                years.
+              </p>
+            </div>
+            <h2 className="text-2xl font-bold">
+              Access to your Personal Information
+            </h2>
+            <div className="flex-col space-y-4">
+              <p className="text-xl">
+                You may access the Personal Information we hold about you and to
+                update and/or correct it, subject to certain exceptions. If you
+                wish to access your Personal Information, please contact us in
+                writing.
+              </p>
+              <p className="text-xl">
+                WAM Master will not charge any fee for your access request, but
+                may charge an administrative fee for providing a copy of your
+                Personal Information.
+              </p>
+              <p className="text-xl">
+                In order to protect your Personal Information we may require
+                identification from you before releasing the requested
+                information.
+              </p>
+            </div>
+            <h2 className="text-2xl font-bold">
+              Maintaining the Quality of your Personal Information
+            </h2>
+            <div className="flex-col space-y-4">
+              <p className="text-xl">
+                It is an important to us that your Personal Information is up to
+                date. We will take reasonable steps to make sure that your
+                Personal Information is accurate, complete and up-to-date. If
+                you find that the information we have is not up to date or is
+                inaccurate, please advise us as soon as practicable so we can
+                update our records and ensure we can continue to provide quality
+                services to you.
+              </p>
+            </div>
+            <h2 className="text-2xl font-bold">Policy Updates</h2>
+            <div className="flex-col space-y-4">
+              <p className="text-xl">
+                This Policy may change from time to time and is available on our
+                website.
+              </p>
+            </div>
+            <h2 className="text-2xl font-bold">
+              Privacy Policy Complaints and Enquiries
+            </h2>
+            <div className="flex-col space-y-4">
+              <p className="text-xl">
+                If you have any queries or complaints about our Privacy Policy
+                please contact us at:
+              </p>
+              <p className="text-xl">
+                Minh Vo
+                <br />
                 minhvo.business@gmail.com
-              </a>
-            </p>
+                <br />
+                +61 449 182 740
+              </p>
+            </div>
           </div>
         </div>
       </div>
